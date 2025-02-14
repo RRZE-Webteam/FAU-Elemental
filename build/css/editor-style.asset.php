893e5ee0062e6acc4c0052c58f7acb85ad0efba2
Marten Manthey <martenmanthey1995@gmail.com>
2025-02-14 11:19:28 +0100

    Migrate theme color variables from SCSS to CSS custom properties
    - Convert color, spacing, and typography variables from SCSS to CSS custom properties
    - Update core-button.scss to use new CSS custom properties
    - Remove hardcoded color values in favor of semantic variable names
    - Refactor theme.scss to define root-level CSS variables
    - Ensure consistent naming convention for color and typography variables

--- build/css/editor-style.asset.php
+++ build/css/editor-style.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array(), 'version' => '8bc64fab7b3f7884e200');
+<?php return array('dependencies' => array(), 'version' => '79018e0928c55db6a41e');
--- build/css/theme.asset.php
+++ build/css/theme.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array(), 'version' => '12b26306e29e17a23116');
+<?php return array('dependencies' => array(), 'version' => '30e7860a038bc59f9d4a');
