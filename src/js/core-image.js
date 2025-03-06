7cef8aae4e8a4d3a4c84577331dec52290719bb5
Marten Manthey <martenmanthey1995@gmail.com>
2025-03-06 17:38:16 +0100

    #48 Image Komponente: files

---
+++ src/js/core-image.js
@@ -0,0 +1,7 @@
+domReady(() => {
+    wp.blocks.unregisterBlockStyle('core/image', ['default', 'rounded']);
+
+    wp.blocks.registerBlockStyle('core/image', { name: 'large', label: 'Large', isDefault: true });
+    wp.blocks.registerBlockStyle('core/image', { name: 'medium', label: 'Medium', isDefault: false });
+    wp.blocks.registerBlockStyle('core/image', { name: 'small', label: 'Small', isDefault: false });
+});
