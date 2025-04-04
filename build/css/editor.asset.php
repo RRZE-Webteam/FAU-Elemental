96b44a241a59e99110e5be1306d3f8a2370d8aa6
Daniel Böse <daniel.boese@mondula.com>
2025-04-04 17:07:47 +0200

    #4 Galerie: CSS

--- build/css/editor.asset.php
+++ build/css/editor.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array(), 'version' => '968366500dc45ce0bbad');
+<?php return array('dependencies' => array(), 'version' => 'a09f2f288b126deca829');
--- build/css/theme.asset.php
+++ build/css/theme.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array(), 'version' => '6c5e4ab0a5b3f5b28732');
+<?php return array('dependencies' => array(), 'version' => '4dcc81d50adcb3ea2afb');
