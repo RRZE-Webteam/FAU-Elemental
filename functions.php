991f67a3c175e4d8385888e4b508363cebb31bca
Dea Bokshi <deabokshi@gmail.com>
2025-04-10 15:45:33 +0200

    #43 added logo to customizer

--- functions.php
+++ functions.php
@@ -21,3 +21,37 @@ require_once get_template_directory() . '/inc/block-patterns.php';
 
 // Theme settings
 require_once get_template_directory() . '/inc/theme-settings.php';
+
+/**
+ * Add custom logo support
+ */
+function fau_elemental_custom_logo_setup() {
+    add_theme_support('custom-logo', array(
+        'height'      => 100,
+        'width'       => 400,
+        'flex-height' => true,
+        'flex-width'  => true,
+        'header-text' => array('site-title', 'site-description'),
+    ));
+}
+add_action('after_setup_theme', 'fau_elemental_custom_logo_setup');
+
+/**
+ * Add logo settings to customizer
+ */
+function fau_elemental_customize_register($wp_customize) {
+
+    // Add logo setting
+    $wp_customize->add_setting('fau_elemental_logo', array(
+        'default'           => '',
+        'sanitize_callback' => 'esc_url_raw',
+    ));
+
+    // Add logo control
+    $wp_customize->add_control(new WP_Customize_Image_Control($wp_customize, 'fau_elemental_logo', array(
+        'label'    => __('Upload Logo', 'fau-elemental'),
+        'section'  => 'fau_elemental_logo_section',
+        'settings' => 'fau_elemental_logo',
+    )));
+}
+add_action('customize_register', 'fau_elemental_customize_register');
