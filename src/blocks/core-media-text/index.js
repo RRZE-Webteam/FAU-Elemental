66782f86614278ba61642c8e469cc38c92938e71
Marten Manthey <martenmanthey1995@gmail.com>
2025-04-22 11:07:04 +0200

    #13: default media-text heading lvl 2

--- src/blocks/core-media-text/index.js
+++ src/blocks/core-media-text/index.js
@@ -19,7 +19,7 @@ registerBlockVariation( 'core/media-text', {
 	innerBlocks: [
 		[
 			'core/heading',
-			{ placeholder: __( 'Heading', 'fau-elemental' ), level: 3 },
+			{ placeholder: __( 'Heading', 'fau-elemental' ), level: 2 },
 		],
 		[ 'core/paragraph', { placeholder: __( 'Content', 'fau-elemental' ) } ],
 	],
