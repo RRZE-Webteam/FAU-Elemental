242c3a76a0ff5a9180e28da76429a00cbb3779d8
Dea Bokshi <deabokshi@gmail.com>
2025-04-28 16:13:27 +0200

    small fix

--- build/fau-featured-event-teaser/index.asset.php
+++ build/fau-featured-event-teaser/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => '990f991fe48664c9700f');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => '5108df912d6d2579cbb6');
