821dad53172b7d66babc13f61f7bc9eda6c22b37
Daniel Böse <daniel.boese@mondula.com>
2025-05-06 16:03:40 +0200

    #5 CSS

--- build/css/editor.asset.php
+++ build/css/editor.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array(), 'version' => 'a830e833e532c4d3073f');
+<?php return array('dependencies' => array(), 'version' => 'bcd79b694fd1aef1650e');
--- build/css/theme.asset.php
+++ build/css/theme.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array(), 'version' => 'e3d58dd3c1ed09002e39');
+<?php return array('dependencies' => array(), 'version' => '2f696298a4c42f928c92');
