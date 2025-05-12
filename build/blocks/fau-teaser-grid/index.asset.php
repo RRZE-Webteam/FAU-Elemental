dea72cd3fe89fef69d57aecbe00d28b953721b04
FitoreGashi <fitore.gashi@mondula.com>
2025-05-12 15:35:08 +0200

    make the category link on edit

--- build/blocks/fau-teaser-grid/index.asset.php
+++ build/blocks/fau-teaser-grid/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-core-data', 'wp-data', 'wp-i18n'), 'version' => 'b2854f4d9f28fd28b704');
+<?php return array('dependencies' => array('react', 'react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-core-data', 'wp-data', 'wp-i18n'), 'version' => '5f767825a435c7ec06d6');
