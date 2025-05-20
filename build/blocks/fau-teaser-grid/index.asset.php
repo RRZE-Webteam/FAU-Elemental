8ee7ecb2f25412422fb1767a8bd7d0e3e4b71824
Daniel Böse <daniel.boese@mondula.com>
2025-05-20 18:19:12 +0200

    #51 #52 #53: l2s & 2sl layout repeat

--- build/blocks/fau-teaser-grid/index.asset.php
+++ build/blocks/fau-teaser-grid/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => '09d497497887124df335');
+<?php return array('dependencies' => array('react', 'react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => 'd75ee3ea2c9758ed5260');
