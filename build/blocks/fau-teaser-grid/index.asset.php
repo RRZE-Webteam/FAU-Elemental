62475f3dd6d777b316a8b260048ac0584ae40c57
FitoreGashi <fitore.gashi@mondula.com>
2025-05-23 13:21:03 +0200

    #52 #51 #53 conent type Either 'post' or 'page' type

--- build/blocks/fau-teaser-grid/index.asset.php
+++ build/blocks/fau-teaser-grid/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => '2b01798da0ef61715a36');
+<?php return array('dependencies' => array('react', 'react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => '929f854f3a40e0908a73');
