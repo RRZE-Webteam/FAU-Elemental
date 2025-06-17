5deeba91b6ca6db61a5c4e259ed05a1d67b2973e
FitoreGashi <fitore.gashi@mondula.com>
2025-06-17 11:54:32 +0200

    remove build

--- build/blocks/fau-list-filters/editor.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array(), 'version' => 'a7d576e0ec59e21c0053');
--- build/blocks/fau-list-filters/view.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array(), 'version' => '6f7b566b5ae3d942f882');
