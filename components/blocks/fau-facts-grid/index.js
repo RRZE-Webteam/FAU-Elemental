4c7048cb70c79b552c2a7f21c1754f534b736e26
Dea Bokshi <deabokshi@gmail.com>
2025-06-20 11:10:54 +0200

    removed render

--- components/blocks/fau-facts-grid/index.js
+++ components/blocks/fau-facts-grid/index.js
@@ -1,8 +1,107 @@
 import { registerBlockType } from '@wordpress/blocks';
+import { useBlockProps } from '@wordpress/block-editor';
+import { __ } from '@wordpress/i18n';
+
 import Edit from './edit';
 import metadata from './block.json';
 
+/**
+ * Save function - generates static HTML
+ */
+function Save( { attributes } ) {
+	const { facts } = attributes;
+	const blockProps = useBlockProps.save();
+
+	return (
+		<div { ...blockProps }>
+			<div className="fau-facts-grid">
+				{ facts && facts.length > 0 && (
+					<div className="fau-facts-grid-items">
+						{ facts.map( ( fact, index ) => {
+							if ( ! fact || typeof fact !== 'object' ) {
+								return null;
+							}
+
+							const {
+								text = '',
+								iconUrl = '',
+								link = '',
+								showLink = false,
+							} = fact;
+
+							const hasLinkClass =
+								link && showLink ? ' has-link' : '';
+
+							return (
+								<div
+									key={ index }
+									className={ `fau-facts-grid-item${ hasLinkClass }` }
+								>
+									<div className="fau-facts-grid-item-icon">
+										{ iconUrl ? (
+											<img src={ iconUrl } alt="" />
+										) : (
+											<img
+												src="/wp-content/themes/fau-elemental/assets/images/fact-icon.png"
+												alt=""
+											/>
+										) }
+									</div>
+									<div className="fau-facts-grid-item-content">
+										{ text && (
+											<div
+												className="fau-facts-grid-item-text"
+												dangerouslySetInnerHTML={ {
+													__html: text,
+												} }
+											/>
+										) }
+
+										{ link && showLink && (
+											<div className="wp-block-buttons">
+												<div className="wp-block-button is-style-tertiary">
+													<a
+														className="wp-block-button__link"
+														href={ link }
+													>
+														{ __(
+															'More',
+															'fau-elemental'
+														) }
+													</a>
+												</div>
+											</div>
+										) }
+									</div>
+								</div>
+							);
+						} ) }
+					</div>
+				) }
+			</div>
+		</div>
+	);
+}
+
 registerBlockType( metadata.name, {
+	...metadata,
 	edit: Edit,
-	save: () => null, // Dynamic block - rendered via PHP
+	save: Save,
+	deprecated: [
+		{
+			// Previous version that used dynamic rendering
+			attributes: metadata.attributes,
+			save: () => null, // Was a dynamic block
+			migrate( attributes ) {
+				// No migration needed, just use the same attributes
+				return attributes;
+			},
+			// eslint-disable-next-line no-unused-vars
+			isEligible( attributes, innerBlocks ) {
+				// This deprecated version applies to blocks that have no saved content
+				// but have attributes (i.e., were previously dynamic blocks)
+				return true;
+			},
+		},
+	],
 } );
--- components/patterns/facts-grid-with-header/pattern.php
+++ components/patterns/facts-grid-with-header/pattern.php
@@ -1,13 +1,13 @@
 <?php
 
 /**
- * Title: Facts Grid mit Dachzeile
+ * Title: Facts Grid with Headline
  * Slug: fau-elemental/facts-grid-with-header
  * Categories: fau-elemental
  * Viewport Width: 1376
  * Block Types: core/post-content
  * Post Types: page
- * Description: Ein Facts Grid mit 4 Fakten und einer Dachzeile darüber
+ * Description: Facts grid with 4 facts and a headline
  */
 
 ?>
@@ -18,6 +18,8 @@
     <header class="wp-block-fau-elemental-fau-meta-headline">Dachzeile</header>
     <!-- /wp:fau-elemental/fau-meta-headline -->
     
-    <!-- wp:fau-elemental/fau-facts-grid {"facts":[{"text":"<strong>270+</strong><br>Studiengänge","iconUrl":"","iconId":null,"link":"","showLink":false},{"text":"<strong>40.000+</strong><br>Studierende","iconUrl":"","iconId":null,"link":"","showLink":false},{"text":"<strong>5</strong><br>Fakultäten","iconUrl":"","iconId":null,"link":"","showLink":false},{"text":"<strong>1743</strong><br>Gegründet","iconUrl":"","iconId":null,"link":"","showLink":false}]} /-->
+    <!-- wp:fau-elemental/fau-facts-grid {"facts":[{"text":"<strong>270+</strong><br>Studiengänge","iconUrl":"","iconId":null,"link":"","showLink":false},{"text":"<strong>40.000+</strong><br>Studierende","iconUrl":"","iconId":null,"link":"","showLink":false},{"text":"<strong>5</strong><br>Fakultäten","iconUrl":"","iconId":null,"link":"","showLink":false},{"text":"<strong>1743</strong><br>Gegründet","iconUrl":"","iconId":null,"link":"","showLink":false}]} -->
+    <div class="wp-block-fau-elemental-fau-facts-grid"><div class="fau-facts-grid"><div class="fau-facts-grid-items"><div class="fau-facts-grid-item"><div class="fau-facts-grid-item-icon"><img src="/wp-content/themes/fau-elemental/assets/images/fact-icon.png" alt=""/></div><div class="fau-facts-grid-item-content"><div class="fau-facts-grid-item-text"><strong>270+</strong><br>Studiengänge</div></div></div><div class="fau-facts-grid-item"><div class="fau-facts-grid-item-icon"><img src="/wp-content/themes/fau-elemental/assets/images/fact-icon.png" alt=""/></div><div class="fau-facts-grid-item-content"><div class="fau-facts-grid-item-text"><strong>40.000+</strong><br>Studierende</div></div></div><div class="fau-facts-grid-item"><div class="fau-facts-grid-item-icon"><img src="/wp-content/themes/fau-elemental/assets/images/fact-icon.png" alt=""/></div><div class="fau-facts-grid-item-content"><div class="fau-facts-grid-item-text"><strong>5</strong><br>Fakultäten</div></div></div><div class="fau-facts-grid-item"><div class="fau-facts-grid-item-icon"><img src="/wp-content/themes/fau-elemental/assets/images/fact-icon.png" alt=""/></div><div class="fau-facts-grid-item-content"><div class="fau-facts-grid-item-text"><strong>1743</strong><br>Gegründet</div></div></div></div></div></div>
+    <!-- /wp:fau-elemental/fau-facts-grid -->
 </div>
 <!-- /wp:group --> 
