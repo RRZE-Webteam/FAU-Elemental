e9f7b5f7758da1e93f0bcd1472422b8f0ccbe229
Marten Manthey <martenmanthey1995@gmail.com>
2025-06-23 14:37:09 +0200

    pattern and demo file for mini list (file) #41

---
+++ components/patterns/mini-list-file/pattern.php
@@ -0,0 +1,53 @@
+<?php
+
+/**
+ * Title: Mini List: File
+ * Slug: fau-elemental/mini-list-file
+ * Categories: fau-elemental
+ * Description: A list of files with a headline and a button to view more files.
+ * Block Types: core/post-content
+ * Post Types: page
+ */
+?>
+
+<!-- wp:fau-elemental/fau-meta-headline -->
+<header class="wp-block-fau-elemental-fau-meta-headline">Dateien</header>
+<!-- /wp:fau-elemental/fau-meta-headline -->
+
+<!-- wp:spacer {"height":"40px","width":"0px","style":{"layout":[]}} -->
+<div style="height:40px;width:0px" aria-hidden="true" class="wp-block-spacer"></div>
+<!-- /wp:spacer -->
+
+<!-- wp:spacer {"height":"12px","width":"0px","style":{"layout":[]}} -->
+<div style="height:12px;width:0px" aria-hidden="true" class="wp-block-spacer"></div>
+<!-- /wp:spacer -->
+
+<!-- wp:file {"id":0,"href":"<?php echo esc_url(get_theme_file_uri('assets/images/Dateiname.png')); ?>","coverImage":{"id":0,"url":"<?php echo esc_url(get_theme_file_uri('assets/images/Dateiname.png')); ?>","alt":""},"fileDetails":{"filename":"Dateiname","filesize":744,"mime_type":"image/png"}} -->
+<div class="wp-block-file"><div class="wp-block-file__content-wrapper"><figure class="file-cover-image" aria-label="Cover image for file"><img src="<?php echo esc_url(get_theme_file_uri('assets/images/Dateiname.png')); ?>" alt=""/></figure><section class="wp-block-file"><div class="file-content"><div class="wp-block-file"><a id="wp-block-file--media-c3cd9330-06db-45ba-9c01-c31451df1dee" href="<?php echo esc_url(get_theme_file_uri('assets/images/Dateiname.png')); ?>" aria-label="Dateiname Download" aria-describedby="wp-block-file--media-c3cd9330-06db-45ba-9c01-c31451df1dee">File Titel</a><a href="<?php echo esc_url(get_theme_file_uri('assets/images/Dateiname.png')); ?>" class="wp-block-file__button wp-element-button" download aria-describedby="wp-block-file--media-c3cd9330-06db-45ba-9c01-c31451df1dee" aria-label="Dateiname Download" role="button"></a></div><div class="file-info-wrapper"><dl class="file-info-list"><div class="file-info-item"><dt class="file-info-term">File Name</dt><dd class="file-info-definition">Dateiname</dd></div><div class="file-info-item"><dt class="file-info-term">File Size</dt><dd class="file-info-definition">744 Bytes</dd></div><div class="file-info-item"><dt class="file-info-term">File Type</dt><dd class="file-info-definition">PNG</dd></div></dl></div></div></section></div></div>
+<!-- /wp:file -->
+
+<!-- wp:spacer {"height":"24px"} -->
+<div style="height:24px" aria-hidden="true" class="wp-block-spacer"></div>
+<!-- /wp:spacer -->
+
+<!-- wp:file {"id":0,"href":"<?php echo esc_url(get_theme_file_uri('assets/images/Dateiname.png')); ?>","coverImage":{"id":0,"url":"<?php echo esc_url(get_theme_file_uri('assets/images/Dateiname.png')); ?>","alt":""},"fileDetails":{"filename":"Dateiname","filesize":744,"mime_type":"image/png"}} -->
+<div class="wp-block-file"><div class="wp-block-file__content-wrapper"><figure class="file-cover-image" aria-label="Cover image for file"><img src="<?php echo esc_url(get_theme_file_uri('assets/images/Dateiname.png')); ?>" alt=""/></figure><section class="wp-block-file"><div class="file-content"><div class="wp-block-file"><a id="wp-block-file--media-347149d2-0503-4190-9656-3f473cee70ca" href="<?php echo esc_url(get_theme_file_uri('assets/images/Dateiname.png')); ?>" aria-label="Dateiname Download" aria-describedby="wp-block-file--media-347149d2-0503-4190-9656-3f473cee70ca">File Titel</a><a href="<?php echo esc_url(get_theme_file_uri('assets/images/Dateiname.png')); ?>" class="wp-block-file__button wp-element-button" download aria-describedby="wp-block-file--media-347149d2-0503-4190-9656-3f473cee70ca" aria-label="Dateiname Download" role="button"></a></div><div class="file-info-wrapper"><dl class="file-info-list"><div class="file-info-item"><dt class="file-info-term">File Name</dt><dd class="file-info-definition">Dateiname</dd></div><div class="file-info-item"><dt class="file-info-term">File Size</dt><dd class="file-info-definition">744 Bytes</dd></div><div class="file-info-item"><dt class="file-info-term">File Type</dt><dd class="file-info-definition">PNG</dd></div></dl></div></div></section></div></div>
+<!-- /wp:file -->
+
+<!-- wp:spacer {"height":"24px"} -->
+<div style="height:24px" aria-hidden="true" class="wp-block-spacer"></div>
+<!-- /wp:spacer -->
+
+<!-- wp:file {"id":0,"href":"<?php echo esc_url(get_theme_file_uri('assets/images/Dateiname.png')); ?>","fileDetails":{"filename":"Dateiname","filesize":744,"mime_type":"image/png"}} -->
+<div class="wp-block-file"><div class="wp-block-file__content-wrapper"><figure class="file-cover-image" aria-label="Cover image for file"></figure><section class="wp-block-file"><div class="file-content"><div class="wp-block-file"><a id="wp-block-file--media-d47be20e-d0d5-44b0-b891-0ce21422397a" href="<?php echo esc_url(get_theme_file_uri('assets/images/Dateiname.png')); ?>" aria-label="Dateiname Download" aria-describedby="wp-block-file--media-d47be20e-d0d5-44b0-b891-0ce21422397a">Dateiname</a><a href="<?php echo esc_url(get_theme_file_uri('assets/images/Dateiname.png')); ?>" class="wp-block-file__button wp-element-button" download aria-describedby="wp-block-file--media-d47be20e-d0d5-44b0-b891-0ce21422397a" aria-label="Dateiname Download" role="button"></a></div><div class="file-info-wrapper"><dl class="file-info-list"><div class="file-info-item"><dt class="file-info-term">File Name</dt><dd class="file-info-definition">Dateiname</dd></div><div class="file-info-item"><dt class="file-info-term">File Size</dt><dd class="file-info-definition">744 Bytes</dd></div><div class="file-info-item"><dt class="file-info-term">File Type</dt><dd class="file-info-definition">PNG</dd></div></dl></div></div></section></div></div>
+<!-- /wp:file -->
+
+<!-- wp:spacer {"height":"40px"} -->
+<div style="height:40px" aria-hidden="true" class="wp-block-spacer"></div>
+<!-- /wp:spacer -->
+
+<!-- wp:buttons {"layout":{"type":"flex","justifyContent":"right"}} -->
+<div class="wp-block-buttons"><!-- wp:button -->
+<div class="wp-block-button"><a class="wp-block-button__link wp-element-button">Weitere Inhalte</a></div>
+<!-- /wp:button --></div>
+<!-- /wp:buttons -->
