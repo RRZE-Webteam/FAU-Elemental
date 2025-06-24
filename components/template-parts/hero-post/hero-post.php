83a3fe69ac07c1eb98e44dd21d76f5d6673faa57
Marten Manthey <martenmanthey1995@gmail.com>
2025-06-24 15:48:31 +0200

    post hero fix

--- components/template-parts/hero-post/hero-post.php
+++ components/template-parts/hero-post/hero-post.php
@@ -12,11 +12,11 @@
         
         <div class="post-meta-top">
             <div class="wp-block-post-date">
-                <time datetime="<?php echo get_the_date('c'); ?>"><?php echo get_the_date(); ?></time>
+                <time datetime="<?php echo get_the_date('c'); ?>"><?php echo get_the_date('j. F Y'); ?></time>
             </div>
             
             <?php if (has_category()) : ?>
-            <p class="post-categories-separator">–</p>
+            <span class="post-categories-separator">–</span>
             
             <div class="post-categories"><?php 
                 $categories = get_the_category();
@@ -42,12 +42,43 @@
     </div>
     
     <?php if (has_post_thumbnail()) : 
-        // Render the post-featured-image block
+        // Get featured image data
+        $thumbnail_id = get_post_thumbnail_id();
+        $image_src = wp_get_attachment_image_src($thumbnail_id, 'large');
+        $image_alt = get_post_meta($thumbnail_id, '_wp_attachment_image_alt', true);
+        
+        // Get image caption from attachment
+        $attachment = get_post($thumbnail_id);
+        $caption = $attachment->post_excerpt;
+        
+        // Create the inner HTML for the image
+        $inner_html = sprintf(
+            '<figure class="wp-block-image size-large is-style-large has-overlay"><img src="%s" alt="%s" class="wp-image-%d"/>',
+            esc_url($image_src[0]),
+            esc_attr($image_alt),
+            $thumbnail_id
+        );
+        
+        // Add caption if it exists
+        if (!empty($caption)) {
+            $inner_html .= sprintf('<figcaption class="wp-element-caption">%s</figcaption>', esc_html($caption));
+        }
+        
+        $inner_html .= '</figure>';
+        
+        // Render the core/image block
         echo render_block(array(
-            'blockName' => 'core/post-featured-image',
+            'blockName' => 'core/image',
             'attrs' => array(
-                'className' => 'wp-block-image is-style-large'
+                'id' => $thumbnail_id,
+                'sizeSlug' => 'large',
+                'linkDestination' => 'none',
+                'align' => 'full',
+                'className' => 'is-style-large has-overlay'
             ),
+            'innerBlocks' => array(),
+            'innerHTML' => $inner_html,
+            'innerContent' => array($inner_html)
         ));
     endif; ?>
 </div> 
