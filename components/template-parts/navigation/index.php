4e5b8fb14f5f343b99ba3f165f0e8b66b9608c01
Dea Bokshi <deabokshi@gmail.com>
2025-07-02 14:53:49 +0200

    addressed comments

--- components/template-parts/navigation/index.php
+++ components/template-parts/navigation/index.php
@@ -11,6 +11,3 @@ if (!defined('ABSPATH')) {
 
 // Include the unified menu modal system first
 require_once __DIR__ . '/menu-modal-config.php';
-
-// Navigation template parts are loaded via get_template_part() calls
-// No initialization needed as they are pure template files 
