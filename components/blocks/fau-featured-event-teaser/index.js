2743a564324a07c444000b72911065bb4d01411f
Nicolas Hollmann <nico@hokli-software.de>
2025-07-03 17:05:17 +0200

    Small naming fix

--- components/blocks/fau-featured-event-teaser/index.js
+++ components/blocks/fau-featured-event-teaser/index.js
@@ -1,10 +1,10 @@
 import { registerBlockType } from '@wordpress/blocks';
 import Edit from './edit';
-import save from './save';
+import Save from './save';
 import metadata from './block.json';
 
 registerBlockType( metadata.name, {
 	...metadata,
 	edit: Edit,
-	save,
+	save: Save,
 } );
