98821ec9ae3af87ad066be570a7f654860212139
Marten Manthey <martenmanthey1995@gmail.com>
2025-07-04 14:16:43 +0200

    fix spacing in big button patterns

--- components/patterns/big-buttons-faculties/pattern.php
+++ components/patterns/big-buttons-faculties/pattern.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * Title: Big Buttons (Faculties Showcase)
  * Slug: fau-elemental/big-buttons-faculties-showcase
@@ -11,18 +12,20 @@
 
 ?>
 
-<!-- wp:group {"layout":{"type":"constrained","justifyContent":"left"}} -->
-<div class="wp-block-group"><!-- wp:fau-elemental/fau-meta-headline {"headline":"Fakultäten","id":""} -->
-<div class="wp-block-fau-elemental-fau-meta-headline" id="headline-">Fakultäten</div>
-<!-- /wp:fau-elemental/fau-meta-headline -->
-<!-- wp:heading -->
-<h2 class="wp-block-heading">Headline Fakultäten</h2>
-<!-- /wp:heading -->
+<!-- wp:group {"className":"big-buttons-faculties"} -->
+<div class="wp-block-group big-buttons-faculties">
+    <!-- wp:fau-elemental/fau-meta-headline {"headline":"Fakultäten","id":""} -->
+    <div class="wp-block-fau-elemental-fau-meta-headline" id="headline-">Fakultäten</div>
+    <!-- /wp:fau-elemental/fau-meta-headline -->
 
-<!-- wp:paragraph -->
-<p>Magna neque purus eget hendrerit cras quam aliquet. Cursus diam ultricies nunc consectetur neque ultrices maecenas. Pellentesque dictum sem dolor sed laoreet vehicula arcu etiam elit. Faucibus ut.</p>
-<!-- /wp:paragraph -->
+    <!-- wp:heading -->
+    <h2 class="wp-block-heading">Headline Fakultäten</h2>
+    <!-- /wp:heading -->
 
-<!-- wp:fau-elemental/fau-big-button {"teaserSize":"large","items":[{"id":"1532d07d-09da-4e91-865e-0ef20ed88a3b","title":"Philosophische Fakultät und Fachbereich Theologie","description":"Tristique maecenas nibh lorem ultricies in elit. Vitae volutpat mi nascetur fermentum.","url":"#","facultyColor":"phil"},{"id":"4ca5e110-9fa7-48b1-9109-75a15db6a1a4","title":"Rechts- und Wirtschaftswissenschaftliche Fakultät","description":"Sed id sollicitudin viverra gravida. In varius semper faucibus fermentum. Praesent sed.","url":"#","facultyColor":"rw"},{"id":"032ea5dd-ef42-42e3-a78b-9de5f6a54886","title":"Medizinische Fakultät","description":"Iaculis lacinia eget ipsum id diam faucibus mi turpis sed. Nulla arcu hendrerit vitae nibh vitae. A odio aliquet vitae rhoncus.","url":"Naturwissenschaftliche Fakultät","facultyColor":"med"},{"id":"ca409d8f-a44c-45cf-8592-6788bae89900","title":"Naturwissenschaftliche Fakultät","description":"Sed id sollicitudin viverra gravida. In varius semper faucibus fermentum. Praesent sed.","url":"#","facultyColor":"nat"},{"id":"e9afd77b-bf20-444f-a8d6-3f3e58206a0d","title":"Technische Fakultät","description":"Cras nunc in erat est. Rhoncus adipiscing at habitant bibendum commodo ac sit convallis.","url":"#","facultyColor":"tf"}]} /-->
+    <!-- wp:paragraph -->
+    <p>Magna neque purus eget hendrerit cras quam aliquet. Cursus diam ultricies nunc consectetur neque ultrices maecenas. Pellentesque dictum sem dolor sed laoreet vehicula arcu etiam elit. Faucibus ut.</p>
+    <!-- /wp:paragraph -->
+
+    <!-- wp:fau-elemental/fau-big-button {"teaserSize":"large","items":[{"id":"1532d07d-09da-4e91-865e-0ef20ed88a3b","title":"Philosophische Fakultät und Fachbereich Theologie","description":"Tristique maecenas nibh lorem ultricies in elit. Vitae volutpat mi nascetur fermentum.","url":"#","facultyColor":"phil"},{"id":"4ca5e110-9fa7-48b1-9109-75a15db6a1a4","title":"Rechts- und Wirtschaftswissenschaftliche Fakultät","description":"Sed id sollicitudin viverra gravida. In varius semper faucibus fermentum. Praesent sed.","url":"#","facultyColor":"rw"},{"id":"032ea5dd-ef42-42e3-a78b-9de5f6a54886","title":"Medizinische Fakultät","description":"Iaculis lacinia eget ipsum id diam faucibus mi turpis sed. Nulla arcu hendrerit vitae nibh vitae. A odio aliquet vitae rhoncus.","url":"Naturwissenschaftliche Fakultät","facultyColor":"med"},{"id":"ca409d8f-a44c-45cf-8592-6788bae89900","title":"Naturwissenschaftliche Fakultät","description":"Sed id sollicitudin viverra gravida. In varius semper faucibus fermentum. Praesent sed.","url":"#","facultyColor":"nat"},{"id":"e9afd77b-bf20-444f-a8d6-3f3e58206a0d","title":"Technische Fakultät","description":"Cras nunc in erat est. Rhoncus adipiscing at habitant bibendum commodo ac sit convallis.","url":"#","facultyColor":"tf"}]} /-->
 </div>
 <!-- /wp:group -->
