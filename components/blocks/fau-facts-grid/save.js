9e3cecc2c9abd8b91e67640b0e8496d63b8156b7
Daniel Böse <daniel.boese@mondula.com>
2025-07-07 16:13:37 +0200

    removed 2 unnecessary divs

--- components/blocks/fau-facts-grid/save.js
+++ components/blocks/fau-facts-grid/save.js
@@ -10,70 +10,65 @@ export default function Save( { attributes } ) {
 
 	return (
 		<div { ...blockProps }>
-			<div className="fau-facts-grid">
-				{ facts && facts.length > 0 && (
-					<div className="fau-facts-grid-items">
-						{ facts.map( ( fact, index ) => {
-							if ( ! fact || typeof fact !== 'object' ) {
-								return null;
-							}
+			{ facts &&
+				facts.map( ( fact, index ) => {
+					if ( ! fact || typeof fact !== 'object' ) {
+						return null;
+					}
 
-							const { text = '', iconUrl = '', link = '' } = fact;
+					const { text = '', iconUrl = '', link = '' } = fact;
 
-							// Don't render fact if no text is provided
-							if ( ! text || text.trim() === '' ) {
-								return null;
-							}
+					// Don't render fact if no text is provided
+					if ( ! text || text.trim() === '' ) {
+						return null;
+					}
 
-							const hasLinkClass = link ? ' has-link' : '';
+					const hasLinkClass = link ? ' has-link' : '';
 
-							return (
-								<div
-									key={ index }
-									className={ `fau-facts-grid-item${ hasLinkClass }` }
-								>
-									<div className="fau-facts-grid-item-icon">
-										{ iconUrl ? (
-											<img src={ iconUrl } alt="" />
-										) : (
-											<img
-												src={ `${ window.fauElemental?.themeUrl }/assets/images/fact-icon.png` }
-												alt=""
-											/>
-										) }
+					return (
+						<div
+							key={ index }
+							className={ `fau-facts-grid-item${ hasLinkClass }` }
+						>
+							<div className="fau-facts-grid-item-icon">
+								{ iconUrl ? (
+									<img src={ iconUrl } alt="" />
+								) : (
+									<img
+										src={ `${ window.fauElemental?.themeUrl }/assets/images/fact-icon.png` }
+										alt=""
+									/>
+								) }
+							</div>
+							<div className="fau-facts-grid-item-content">
+								{ text && (
+									<div
+										className="fau-facts-grid-item-text"
+										dangerouslySetInnerHTML={ {
+											__html: text,
+										} }
+									/>
+								) }
+
+								{ link && (
+									<div className="wp-block-buttons">
+										<div className="wp-block-button is-style-tertiary">
+											<a
+												className="wp-block-button__link"
+												href={ link }
+											>
+												{ __(
+													'More',
+													'fau-elemental'
+												) }
+											</a>
+										</div>
 									</div>
-									<div className="fau-facts-grid-item-content">
-										{ text && (
-											<div
-												className="fau-facts-grid-item-text"
-												dangerouslySetInnerHTML={ {
-													__html: text,
-												} }
-											/>
-										) }
-
-										{ link && (
-											<div className="wp-block-buttons">
-												<div className="wp-block-button is-style-tertiary">
-													<a
-														className="wp-block-button__link"
-														href={ link }
-													>
-														{ __(
-															'More',
-															'fau-elemental'
-														) }
-													</a>
-												</div>
-											</div>
-										) }
-									</div>
-								</div>
-							);
-						} ) }
-					</div>
-				) }
-			</div>
+								) }
+							</div>
+						</div>
+					);
+				} ) }
 		</div>
 	);
 }
