a3150dfd5e77eb8eb2207cc55a57e50529701d29
Marten Manthey <martenmanthey1995@gmail.com>
2025-07-10 10:32:54 +0200

    big teaser pattern, group block layout fix

--- components/core-blocks/group/index.js
+++ components/core-blocks/group/index.js
@@ -3,8 +3,8 @@ import {
 	unregisterBlockVariation,
 } from '@wordpress/blocks';
 import { __ } from '@wordpress/i18n';
+import { addFilter } from '@wordpress/hooks';
 
-// Register dark style for core/group block
 wp.domReady( () => {
 	registerBlockStyle( 'core/group', {
 		name: 'dark',
@@ -12,8 +12,24 @@ wp.domReady( () => {
 		isDefault: false,
 	} );
 
-	// unregister the stack block variation
 	unregisterBlockVariation( 'core/group', 'group-row' );
 	unregisterBlockVariation( 'core/group', 'group-stack' );
 	unregisterBlockVariation( 'core/group', 'group-grid' );
 } );
+
+addFilter(
+	'blocks.registerBlockType',
+	'fau-elemental/edit-group-block-settings',
+	function ( settings, name ) {
+		if ( name !== 'core/group' ) {
+			return settings;
+		}
+
+		settings.supports = {
+			...settings.supports,
+			layout: false,
+		};
+
+		return settings;
+	}
+);
