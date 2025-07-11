25be097102e85c2928c57528ee5c2f8b395e8a4c
Dea Bokshi <deabokshi@gmail.com>
2025-07-11 15:51:00 +0200

    revert menu burger icon

--- components/template-parts/navigation/main-navigation.php
+++ components/template-parts/navigation/main-navigation.php
@@ -44,7 +44,11 @@ $has_website_menu = fau_elemental_has_website_menu();
             <?php if ($has_website_menu): ?>
                 <button class="main-navigation__toggle menu-modal__open-btn" aria-expanded="false" aria-controls="menu-website-modal" data-modal-target="menu-website-modal">
                     <span class="main-navigation__toggle-text">Menü</span>
-                    <span class="main-navigation__toggle-icon"></span>
+                    <span class="main-navigation__toggle-icon">
+                        <span></span>
+                        <span></span>
+                        <span></span>
+                    </span>
                 </button>
             <?php endif; ?>
         </div>
