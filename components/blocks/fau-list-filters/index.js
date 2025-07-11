e0c2e5a9e2a8caa1f9d4d6453c33617958ab3df3
FitoreGashi <fitore.gashi@mondula.com>
2025-07-11 15:03:09 +0200

    This block is not using the metadate from the block.json like the other blocks (fau-pagination)

--- components/blocks/fau-list-filters/index.js
+++ components/blocks/fau-list-filters/index.js
@@ -1,10 +1,12 @@
 import { registerBlockType } from '@wordpress/blocks';
 import Edit from './edit';
+import metadata from './block.json';
 
 /**
  * Register the FAU List Filters block
  */
-registerBlockType( 'fau-elemental/fau-list-filters', {
+registerBlockType( metadata.name, {
+	...metadata,
 	edit: Edit,
 	save: () => null, // Server-side rendered block
 } );
