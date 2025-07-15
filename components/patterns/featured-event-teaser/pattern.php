aaf32e4654d942e2c0c090b72b9103b95de2697d
Dea Bokshi <deabokshi@gmail.com>
2025-07-15 13:39:53 +0200

    removed id from pattern

--- components/patterns/featured-event-teaser/pattern.php
+++ components/patterns/featured-event-teaser/pattern.php
@@ -10,11 +10,11 @@
 
 ?>
 <!-- wp:group {"layout":{"type":"constrained"}} -->
-<div class="wp-block-group"><!-- wp:fau-elemental/fau-meta-headline {"headline":"Optionale Dachzeile","id":"81dd4501-599c-425b-b6f8-5c814cb41710"} -->
-<div class="wp-block-fau-elemental-fau-meta-headline" id="headline-81dd4501-599c-425b-b6f8-5c814cb41710">Optionale Dachzeile</div>
+<div class="wp-block-group"><!-- wp:fau-elemental/fau-meta-headline {"headline":"Optionale Dachzeile","id":""} -->
+<div class="wp-block-fau-elemental-fau-meta-headline" id="headline-">Optionale Dachzeile</div>
 <!-- /wp:fau-elemental/fau-meta-headline -->
 
 <!-- wp:fau-elemental/featured-event-teaser {"eventTitle":"Hier steht ein Veranstaltungstitel der eine variable länge bietet","eventDescription":"Lorem ipsum dolor sit amet consectetur. Phasellus ut dignissim odio amet scelerisque in tortor faucibus. Tellus massa integer aenean molestie vitae quis egestas integer. Imperdiet lorem porttitor vitae integer risus. Facilisi blandit turpis vestibulum iaculis scelerisque fusce vitae egestas. Arcu et sit lobortis tincidunt felis."} -->
-<div class="wp-block-fau-elemental-featured-event-teaser"><div class="featured-event-content"><div class="content-left"><h2 class="event-title">Hier steht ein Veranstaltungstitel der eine variable länge bietet</h2><p class="event-description">Lorem ipsum dolor sit amet consectetur. Phasellus ut dignissim odio amet scelerisque in tortor faucibus. Tellus massa integer aenean molestie vitae quis egestas integer. Imperdiet lorem porttitor vitae integer risus. Facilisi blandit turpis vestibulum iaculis scelerisque fusce vitae egestas. Arcu et sit lobortis tincidunt felis.</p><div class="wp-block-button"><a class="wp-block-button__link" href="#">Mehr erfahren</a></div></div><div class="content-right"><time class="event-date" datetime=""><span class="date-day">01</span><span class="date-month-year">Oct 2024</span></time></div></div></div>
+<div class="wp-block-fau-elemental-featured-event-teaser"><div class="featured-event-content"><div class="content-left"><h2 class="event-title">Hier steht ein Veranstaltungstitel der eine variable länge bietet</h2><p class="event-description">Lorem ipsum dolor sit amet consectetur. Phasellus ut dignissim odio amet scelerisque in tortor faucibus. Tellus massa integer aenean molestie vitae quis egestas integer. Imperdiet lorem porttitor vitae integer risus. Facilisi blandit turpis vestibulum iaculis scelerisque fusce vitae egestas. Arcu et sit lobortis tincidunt felis.</p><div class="wp-block-button"><a class="wp-block-button__link" href="#">Mehr erfahren</a></div></div><div class="content-right"><time class="event-date" datetime=""><span class="date-day">01</span><span class="date-month-year">Jan 2024</span></time></div></div></div>
 <!-- /wp:fau-elemental/featured-event-teaser --></div>
 <!-- /wp:group -->
