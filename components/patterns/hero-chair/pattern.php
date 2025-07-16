e9426cf1d3e4212b774e9c614bb93078dba9b1c9
Dea Bokshi <deabokshi@gmail.com>
2025-07-16 15:32:35 +0200

    chair pattern

--- components/patterns/hero-chair/pattern.php
+++ components/patterns/hero-chair/pattern.php
@@ -1,16 +1,59 @@
-<!-- wp:group {"tagName":"header","style":{"spacing":{"padding":{"top":"var:preset|spacing|50","bottom":"var:preset|spacing|50"}}},"backgroundColor":"background","layout":{"type":"constrained"}} -->
-<header class="wp-block-group has-background-background-color has-background" style="padding-top:var(--wp--preset--spacing--50);padding-bottom:var(--wp--preset--spacing--50)">
+<?php
+/**
+ * * Title: Hero: Chair
+ * Slug: fau-elemental/hero-chair
+ * Categories: hero, fau-elemental
+ * Website Types: chair
+ * Viewport Width: 1376
+ * Block Types: core/post-content
+ * Post Types: page
+ * Description: Hero pattern for chair websites
+ */
 
-    <!-- wp:group {"layout":{"type":"flex","flexWrap":"nowrap","justifyContent":"space-between","verticalAlignment":"center"}} -->
-    <div class="wp-block-group">
-        <!-- wp:site-title {"level":0} /-->
+?>
+<!-- wp:columns {"templateLock":"all", "className": "hero-chair"} -->
+<div class="wp-block-columns hero-chair">
+    <!-- wp:column {"layout":{"type":"constrained"},"className":"hero-faculty-left"} -->
+    <div class="wp-block-column">
 
-        <!-- wp:navigation {"layout":{"type":"flex","orientation":"horizontal"}} /-->
+        <!-- wp:heading {"level":2,"className":"hero-front-page-title"} -->
+        <h2 class="wp-block-heading hero-front-page-title">Institut für Politische Wissenschaft</h2>
+        <!-- /wp:heading -->
+
+         <!-- wp:group {"className":"hero-mobile-optional"} -->
+         <div class="wp-block-group hero-mobile-optional">
+            <!-- wp:paragraph {"className":"hero-text"} -->
+            <p class="hero-text">Die Naturwissenschaftliche Fakultät der FAU gehört zu den forschungsstärksten naturwissenschaftlichen Fakultäten in Deutschland. Ihre Fachbereiche belegen regelmäßig vorderste Plätze in unterschiedlichsten Rankings.</p>
+            <!-- /wp:paragraph -->
+
+            <!-- wp:buttons {"className":"hero-link-wrapper"} -->
+            <div class="wp-block-buttons hero-link-wrapper">
+                 <!-- wp:button {"className":"hero-link"} -->
+                 <div class="wp-block-button hero-link">
+                     <a class="wp-block-button__link" href="#">Mehr erfahren</a>
+                 </div>
+                 <!-- /wp:button -->
+             </div>
+             <!-- /wp:buttons -->
+        </div>
+        <!-- /wp:group -->
     </div>
-    <!-- /wp:group -->
+    <!-- /wp:column -->
 
-    <!-- wp:heading {"textAlign":"center","level":1} -->
-    <h2 class="wp-block-heading has-text-align-center">TODO: Chair Hero Pattern</h2>
-    <!-- /wp:heading -->
-</header>
-<!-- /wp:group -->
+    <!-- wp:column  -->
+    <div class="wp-block-column" >
+        <!-- wp:cover {"url":"<?php echo esc_url(get_theme_file_uri('assets/images/hero-chair.png')); ?>","dimRatio":0,"contentPosition":"center","className":"is-dark-theme"} -->
+        <div class="wp-block-cover is-dark-theme">
+            <span aria-hidden="true" class="wp-block-cover__background has-background-dim-0 has-background-dim"></span>
+            <img class="wp-block-cover__image-background" alt="" src="<?php echo esc_url(get_theme_file_uri('assets/images/hero-chair.png')); ?>" data-object-fit="cover">
+            <div class="wp-block-cover__inner-container">
+                <!-- wp:paragraph {"align":"center","placeholder":"Write title…","fontSize":"large","className":"hideParagraph"} -->
+                <p class="has-text-align-center has-large-font-size hideParagraph"></p>
+                <!-- /wp:paragraph -->
+            </div>
+        </div>
+        <!-- /wp:cover -->
+    </div>
+    <!-- /wp:column -->
+</div>
+<!-- /wp:columns -->
