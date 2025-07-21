b9539f1dd5bf6549cb498aa3d68bef8bb81af573
Marten Manthey <martenmanthey1995@gmail.com>
2025-07-21 16:05:14 +0200

    hero portal adjustments

--- components/patterns/hero-portal/pattern.php
+++ components/patterns/hero-portal/pattern.php
@@ -13,16 +13,18 @@
 <!-- wp:columns {"align":"wide","templateLock":"all", "className": "hero-portal"} -->
 <div class="wp-block-columns alignwide hero-portal">
     <!-- wp:column {"templateLock":"contentOnly"} -->
-    <div class="wp-block-column" >
-        <!-- wp:post-title {"level":1} /-->
-        <!-- wp:paragraph {"className":"post-description"} -->
-        <p class="post-description">Sie interessieren sich für ein naturwissenschaftliches oder mathematisches Studium? Dann sind Sie an der FAU in Erlangen genau richtig. Die Universität Erlangen-Nürnberg bietet Studiengänge aus allen Bereichen der Naturwissenschaften und Mathematik an – sowohl als grundständige Bachelorstudiengänge als auch weiterführende Masterstudiengänge. Die Studiengänge für Lebensmittelchemie und Pharmazie sowie für das Lehramt schließen mit einem Staatsexamen ab.</p>
+    <div class="wp-block-column">
+        <!-- wp:post-title {"level":1, "lock":{"move":true}} /-->
+        <!-- wp:paragraph {"className":"post-description", "lock":{"move":true}} -->
+        <p class="post-description"><?php echo esc_html__('Are you interested in studying natural sciences or mathematics? Then FAU in Erlangen is the right place for you. Friedrich-Alexander-Universität Erlangen-Nürnberg offers degree programs in all areas of natural sciences and mathematics – both as undergraduate Bachelor’s and advanced Master’s programs. The degree programs in food chemistry, pharmacy, and teaching end with a state examination.', 'fau-elemental'); ?></p>
         <!-- /wp:paragraph -->
 
         <!-- wp:buttons -->
         <div class="wp-block-buttons">
-             <!-- wp:button {"className":"is-style-tertiary"} -->
-             <div class="wp-block-button is-style-tertiary"><a class="wp-block-button__link wp-element-button">Mehr erfahren</a></div>
+            <!-- wp:button {"className":"is-style-tertiary", "lock":{"remove":false}} -->
+            <div class="wp-block-button is-style-tertiary">
+                <a class="wp-block-button__link wp-element-button"><?php echo esc_html__('Learn more', 'fau-elemental'); ?></a>
+            </div>
             <!-- /wp:button -->
         </div>
         <!-- /wp:buttons -->
@@ -30,11 +32,11 @@
     <!-- /wp:column -->
 
     <!-- wp:column  -->
-    <div class="wp-block-column" >
+    <div class="wp-block-column">
         <!-- wp:cover {"url":"<?php echo esc_url(get_theme_file_uri('assets/images/hero-portal.png')); ?>","dimRatio":0,"contentPosition":"center","className":"is-dark-theme"} -->
         <div class="wp-block-cover is-dark-theme">
             <span aria-hidden="true" class="wp-block-cover__background has-background-dim-0 has-background-dim"></span>
-            <img class="wp-block-cover__image-background" alt="" src="<?php echo esc_url(get_theme_file_uri('assets/images/hero-portal.png')); ?>" data-object-fit="cover">
+            <img class="wp-block-cover__image-background" alt="<?php echo esc_attr__('Students at FAU Erlangen-Nürnberg', 'fau-elemental'); ?>" src="<?php echo esc_url(get_theme_file_uri('assets/images/hero-portal.png')); ?>" data-object-fit="cover">
             <div class="wp-block-cover__inner-container">
                 <!-- wp:paragraph {"align":"center","placeholder":"Write title…","fontSize":"large","className":"hideParagraph"} -->
                 <p class="has-text-align-center has-large-font-size hideParagraph"></p>
@@ -46,3 +48,4 @@
     <!-- /wp:column -->
 </div>
 <!-- /wp:columns -->
+ 
