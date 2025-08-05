22abbe7bbe2429f22a5fd4a10613591befc6acc7
Dea Bokshi <deabokshi@gmail.com>
2025-08-05 15:27:21 +0200

    pattern lock

--- components/patterns/featured-event-teaser/pattern.php
+++ components/patterns/featured-event-teaser/pattern.php
@@ -13,10 +13,11 @@ $current_date = date('j n Y'); // j = day without leading zero, n = month withou
 
 ?>
 <!-- wp:group {"layout":{"type":"constrained"},"className":"featured-event-teaser"} -->
-<div class="wp-block-group featured-event-teaser"><!-- wp:fau-elemental/fau-meta-headline {"headline":"Optionale Dachzeile","id":""} -->
+<div class="wp-block-group featured-event-teaser">
+<!-- wp:fau-elemental/fau-meta-headline {"headline":"Optionale Dachzeile","id":"","lock":{"move":true}} -->
 <div class="wp-block-fau-elemental-fau-meta-headline" id="headline-">Optionale Dachzeile</div>
 <!-- /wp:fau-elemental/fau-meta-headline -->
 
-<!-- wp:fau-elemental/featured-event-teaser {"eventTitle":"Hier steht ein Veranstaltungstitel der eine variable länge bietet","eventDescription":"Lorem ipsum dolor sit amet consectetur. Phasellus ut dignissim odio amet scelerisque in tortor faucibus. Tellus massa integer aenean molestie vitae quis egestas integer. Imperdiet lorem porttitor vitae integer risus. Facilisi blandit turpis vestibulum iaculis scelerisque fusce vitae egestas. Arcu et sit lobortis tincidunt felis.","eventDate":"<?php echo $current_date; ?>","buttonText":"Mehr erfahren","buttonUrl":"#"} -->
+<!-- wp:fau-elemental/featured-event-teaser {"eventTitle":"Hier steht ein Veranstaltungstitel der eine variable länge bietet","eventDescription":"Lorem ipsum dolor sit amet consectetur. Phasellus ut dignissim odio amet scelerisque in tortor faucibus. Tellus massa integer aenean molestie vitae quis egestas integer. Imperdiet lorem porttitor vitae integer risus. Facilisi blandit turpis vestibulum iaculis scelerisque fusce vitae egestas. Arcu et sit lobortis tincidunt felis.","eventDate":"<?php echo $current_date; ?>","buttonText":"Mehr erfahren","buttonUrl":"#", "lock":{"move":true}} -->
 <!-- /wp:fau-elemental/featured-event-teaser --></div>
 <!-- /wp:group -->
