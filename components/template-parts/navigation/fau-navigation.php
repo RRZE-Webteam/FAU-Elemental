b3809332223e1b419f9ab81edb83f0eaa80215bd
Marten Manthey <martenmanthey1995@gmail.com>
2025-08-21 11:53:58 +0200

    tweaks

--- components/template-parts/navigation/fau-navigation.php
+++ components/template-parts/navigation/fau-navigation.php
@@ -55,10 +55,5 @@ $website_type = get_theme_mod('faue_website_type', 'fau');
         <div class="fau-language-switcher-wrapper">
             <?php dynamic_sidebar('language-switcher'); ?>
         </div>
-    <?php else: ?>
-        <button type="button" class="language-switcher fau-navigation__button" aria-label="<?php esc_attr_e('Open Language selector', 'fau-elemental'); ?>" aria-expanded="false">
-            <?php esc_html_e('DE', 'fau-elemental'); ?>
-            <span class="fau-navigation__language-switcher-icon"></span>
-        </button>
     <?php endif; ?>
 </nav> 
