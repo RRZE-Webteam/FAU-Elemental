245237ec2995e9fb6aae2e80d40361dff65d40aa
Marten Manthey <martenmanthey1995@gmail.com>
2025-10-02 11:43:52 +0200

    Refactor menu modal configuration by removing unused structure menu registration and updating the structure menu check to utilize a shortcode. Enhance menu rendering to support shortcode output for the structure modal. #309

--- inc/menu-registration.php
+++ inc/menu-registration.php
@@ -24,7 +24,6 @@ function fau_elemental_register_all_menus() {
 			'header_direct_links_menu' => esc_html__('Header Direct Links Menu', 'fau-elemental'),
 			'header_menu_links' => esc_html__('Header Menu Links', 'fau-elemental'),
 			'top_header_nav_services' => esc_html__('Top Header Nav Services', 'fau-elemental'),
-			'top_header_nav_structure' => esc_html__('Top Header Nav Structure', 'fau-elemental'),
 			'search_options_menu' => esc_html__('Search Options Menu', 'fau-elemental'),
 			'footer-menu'            => esc_html__( 'Footer Menu', 'fau-elemental' ),
 			'footer-lists-menu'      => esc_html__( 'Footer Lists Menu', 'fau-elemental' ),
