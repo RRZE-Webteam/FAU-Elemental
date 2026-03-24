b726b585e2d4ff5e7e5eaf08865e9f63420db4d9
Lukas Niebler <58593037+lukasniebler@users.noreply.github.com>
2026-03-24 13:20:27 +0100

    fix(pattern) - Fixes an URL issue inside the mini-list
    Replaces the URL logic inside the mini-list-file pattern to resolve OB Buffering issues. Part of #676

--- components/patterns/mini-list-file/pattern.php
+++ components/patterns/mini-list-file/pattern.php
@@ -9,6 +9,7 @@
  * Post Types: post, page
  */
 
+$demo_url = esc_url(get_theme_file_uri('assets/images/Demo-Cover.webp'));
 ?>
 
 <!-- wp:group {"className":"mini-list-file"} -->
@@ -18,13 +19,13 @@
     <h2 class="wp-block-fau-elemental-fau-meta-headline" id="headline-">Dateien</h2>
     <!-- /wp:fau-elemental/fau-meta-headline -->
 
-    <!-- wp:file {"id":0,"href":"<?php echo esc_url(get_theme_file_uri('assets/images/Demo-Cover.webp')); ?>","coverImage":{"id":0,"url":"<?php echo esc_url(get_theme_file_uri('assets/images/Demo-Cover.webp')); ?>","alt":""},"fileDetails":{"filename":"Demo-Cover","filesize":3680,"mime_type":"image/webp"}} -->
+    <!-- wp:file {"id":0,"href":"<?php echo $demo_url; ?>","coverImage":{"id":0,"url":"<?php echo $demo_url; ?>","alt":""},"fileDetails":{"filename":"Demo-Cover","filesize":3680,"mime_type":"image/webp"}} -->
     <div class="wp-block-file">
         <div class="wp-block-file__content-wrapper">
-            <figure class="file-cover-image" aria-label="Cover image for file"><img src="<?php echo esc_url(get_theme_file_uri('assets/images/Demo-Cover.webp')); ?>" alt=""></figure>
+            <figure class="file-cover-image" aria-label="Cover image for file"><img src="<?php echo $demo_url; ?>" alt=""></figure>
             <section class="wp-block-file">
                 <div class="file-content">
-                    <div class="wp-block-file"><a id="wp-block-file--media-c3cd9330-06db-45ba-9c01-c31451df1dee" href="<?php echo esc_url(get_theme_file_uri('assets/images/Demo-Cover.webp')); ?>" aria-label="Demo-Cover Download" aria-describedby="wp-block-file--media-c3cd9330-06db-45ba-9c01-c31451df1dee">File Titel</a><a href="<?php echo esc_url(get_theme_file_uri('assets/images/Demo-Cover.webp')); ?>" class="wp-block-file__button wp-element-button" download aria-describedby="wp-block-file--media-c3cd9330-06db-45ba-9c01-c31451df1dee" aria-label="Demo-Cover Download" role="button"></a></div>
+                    <div class="wp-block-file"><a id="wp-block-file--media-c3cd9330-06db-45ba-9c01-c31451df1dee" href="<?php echo $demo_url; ?>" aria-label="Demo-Cover Download" aria-describedby="wp-block-file--media-c3cd9330-06db-45ba-9c01-c31451df1dee">File Titel</a><a href="<?php echo $demo_url; ?>" class="wp-block-file__button wp-element-button" download aria-describedby="wp-block-file--media-c3cd9330-06db-45ba-9c01-c31451df1dee" aria-label="Demo-Cover Download" role="button"></a></div>
                     <div class="file-info-wrapper">
                         <dl class="file-info-list">
                             <div class="file-info-item">
@@ -47,13 +48,13 @@
     </div>
     <!-- /wp:file -->
 
-    <!-- wp:file {"id":0,"href":"<?php echo esc_url(get_theme_file_uri('assets/images/Demo-Cover.webp')); ?>","coverImage":{"id":0,"url":"<?php echo esc_url(get_theme_file_uri('assets/images/Demo-Cover.webp')); ?>","alt":""},"fileDetails":{"filename":"Demo-Cover","filesize":3680,"mime_type":"image/webp"}} -->
+    <!-- wp:file {"id":0,"href":"<?php echo $demo_url; ?>","coverImage":{"id":0,"url":"<?php echo $demo_url; ?>","alt":""},"fileDetails":{"filename":"Demo-Cover","filesize":3680,"mime_type":"image/webp"}} -->
     <div class="wp-block-file">
         <div class="wp-block-file__content-wrapper">
-            <figure class="file-cover-image" aria-label="Cover image for file"><img src="<?php echo esc_url(get_theme_file_uri('assets/images/Demo-Cover.webp')); ?>" alt=""></figure>
+            <figure class="file-cover-image" aria-label="Cover image for file"><img src="<?php echo $demo_url; ?>" alt=""></figure>
             <section class="wp-block-file">
                 <div class="file-content">
-                    <div class="wp-block-file"><a id="wp-block-file--media-347149d2-0503-4190-9656-3f473cee70ca" href="<?php echo esc_url(get_theme_file_uri('assets/images/Demo-Cover.webp')); ?>" aria-label="Demo-Cover Download" aria-describedby="wp-block-file--media-347149d2-0503-4190-9656-3f473cee70ca">File Titel</a><a href="<?php echo esc_url(get_theme_file_uri('assets/images/Demo-Cover.webp')); ?>" class="wp-block-file__button wp-element-button" download aria-describedby="wp-block-file--media-347149d2-0503-4190-9656-3f473cee70ca" aria-label="Demo-Cover Download" role="button"></a></div>
+                    <div class="wp-block-file"><a id="wp-block-file--media-347149d2-0503-4190-9656-3f473cee70ca" href="<?php echo $demo_url; ?>" aria-label="Demo-Cover Download" aria-describedby="wp-block-file--media-347149d2-0503-4190-9656-3f473cee70ca">File Titel</a><a href="<?php echo $demo_url; ?>" class="wp-block-file__button wp-element-button" download aria-describedby="wp-block-file--media-347149d2-0503-4190-9656-3f473cee70ca" aria-label="Demo-Cover Download" role="button"></a></div>
                     <div class="file-info-wrapper">
                         <dl class="file-info-list">
                             <div class="file-info-item">
@@ -76,13 +77,13 @@
     </div>
     <!-- /wp:file -->
 
-    <!-- wp:file {"id":0,"href":"<?php echo esc_url(get_theme_file_uri('assets/images/Demo-Cover.webp')); ?>","fileDetails":{"filename":"Demo-Cover","filesize":3680,"mime_type":"image/webp"}} -->
+    <!-- wp:file {"id":0,"href":"<?php echo $demo_url; ?>","fileDetails":{"filename":"Demo-Cover","filesize":3680,"mime_type":"image/webp"}} -->
     <div class="wp-block-file">
         <div class="wp-block-file__content-wrapper">
             <figure class="file-cover-image" aria-label="Cover image for file"></figure>
             <section class="wp-block-file">
                 <div class="file-content">
-                    <div class="wp-block-file"><a id="wp-block-file--media-d47be20e-d0d5-44b0-b891-0ce21422397a" href="<?php echo esc_url(get_theme_file_uri('assets/images/Demo-Cover.webp')); ?>" aria-label="Demo-Cover Download" aria-describedby="wp-block-file--media-d47be20e-d0d5-44b0-b891-0ce21422397a">File Titel</a><a href="<?php echo esc_url(get_theme_file_uri('assets/images/Demo-Cover.webp')); ?>" class="wp-block-file__button wp-element-button" download aria-describedby="wp-block-file--media-d47be20e-d0d5-44b0-b891-0ce21422397a" aria-label="Demo-Cover Download" role="button"></a></div>
+                    <div class="wp-block-file"><a id="wp-block-file--media-d47be20e-d0d5-44b0-b891-0ce21422397a" href="<?php echo $demo_url; ?>" aria-label="Demo-Cover Download" aria-describedby="wp-block-file--media-d47be20e-d0d5-44b0-b891-0ce21422397a">File Titel</a><a href="<?php echo $demo_url; ?>" class="wp-block-file__button wp-element-button" download aria-describedby="wp-block-file--media-d47be20e-d0d5-44b0-b891-0ce21422397a" aria-label="Demo-Cover Download" role="button"></a></div>
                     <div class="file-info-wrapper">
                         <dl class="file-info-list">
                             <div class="file-info-item">
